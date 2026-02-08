d68217e60f25030bcdef76045970f108b2ca53da
Krishna Batra <batrakrishna20@gmail.com>
2026-02-09 00:00:00 +0530

    Day 8 - DSA

---
+++ Day8/day8_q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int power(int x,int n){
+    if(n==0) return 1;
+    return x*power(x,n-1);
+}
+int main(){
+    int x,n;
+    printf("Enter base and exponent: ");
+    scanf("%d %d",&x,&n);
+    printf("Result: %d",power(x,n));
+}
