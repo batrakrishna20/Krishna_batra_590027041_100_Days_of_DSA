f73fb8f7a557348e76737616a82396b8dd995a31
Krishna Batra <batrakrishna20@gmail.com>
2026-03-16 00:00:00 +0530

    Day 43 - DSA

---
+++ Day43/day43_q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *left,*right;
+};
+
+struct node* newNode(int val){
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=val;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+
+int main(){
+    struct node* root=newNode(1);
+    root->left=newNode(2);
+    root->right=newNode(3);
+
+    printf("Root: %d",root->data);
+}
