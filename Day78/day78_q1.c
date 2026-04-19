b2253b386b2649c6d5e1160aea28f2f37de94e61
Krishna Batra <batrakrishna20@gmail.com>
2026-04-20 00:00:00 +0530

    Day 78 - DSA

---
+++ Day78/day78_q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int partition(int a[],int low,int high){
+    int pivot=a[high],i=low-1;
+    for(int j=low;j<high;j++){
+        if(a[j]<pivot){
+            i++;
+            int t=a[i]; a[i]=a[j]; a[j]=t;
+        }
+    }
+    int t=a[i+1]; a[i+1]=a[high]; a[high]=t;
+    return i+1;
+}
+
+void quickSort(int a[],int low,int high){
+    if(low<high){
+        int pi=partition(a,low,high);
+        quickSort(a,low,pi-1);
+        quickSort(a,pi+1,high);
+    }
+}
+
+int main(){
+    int n,a[100];
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    printf("Enter elements: ");
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+
+    quickSort(a,0,n-1);
+
+    printf("Sorted: ");
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
+}
